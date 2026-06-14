d983e61e7f8c6ec71aeccb2097f7e1e029482064
Sariumane <timoreggentin@ymail.com>
2026-06-14 12:12:42 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,7 @@
+using System.Windows;
+
+namespace WpfMvvmMinimal;
+
+public partial class App : Application
+{
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows;
+
+namespace WpfMvvmMinimal.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
