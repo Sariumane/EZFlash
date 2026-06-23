35a6bffb0c6b02019e9a976ae1ee98a1bfa5c700
timor <timor@DESKTOP-QTKV01S>
2026-06-23 03:02:02 +0200

    Settings Fix und Save on Quit für Decks reaktiviert.

--- Models/AppSettings.cs
+++ Models/AppSettings.cs
@@ -9,8 +9,8 @@
         public float GoodStartFactor { get; set; } = 8f;
         public float EasyStartFactor { get; set; } = 24f * 3f;
 
-        public float AgainMultiplier { get; set; } = 1f;
-        public float HardMultiplier { get; set; } = 0.5f;
+        public float AgainMultiplier { get; set; } = 0.5f;
+        public float HardMultiplier { get; set; } = 1f;
         public float GoodMultiplier { get; set; } = 1.2f;
         public float EasyMultiplier { get; set; } = 1.5f;
 
--- Models/GlobalSettings.cs
+++ Models/GlobalSettings.cs
@@ -2,28 +2,30 @@
 {
     static internal class GlobalSettings
     {
+        private static readonly AppSettings Defaults = new();
+
         // Basiseinheit in Stunden
-        public static float BaseUnit { get; set; } = 1f;
+        public static float BaseUnit { get; set; } = Defaults.BaseUnit;
 
         // Basiseinheit in Sekunden
         public static float BaseInterval => BaseUnit * 3600f;
 
-        public static float AgainStartFactor { get; set; } = 1f;
-        public static float HardStartFactor { get; set; } = 2.5f;
-        public static float GoodStartFactor { get; set; } = 8f;
-        public static float EasyStartFactor { get; set; } = 24f * 3f;
+        public static float AgainStartFactor { get; set; } = Defaults.AgainStartFactor;
+        public static float HardStartFactor { get; set; } = Defaults.HardStartFactor;
+        public static float GoodStartFactor { get; set; } = Defaults.GoodStartFactor;
+        public static float EasyStartFactor { get; set; } = Defaults.EasyStartFactor;
 
         public static float AgainStartInterval => BaseInterval * AgainStartFactor;
         public static float HardStartInterval => BaseInterval * HardStartFactor;
         public static float GoodStartInterval => BaseInterval * GoodStartFactor;
         public static float EasyStartInterval => BaseInterval * EasyStartFactor;
 
-        public static float AgainMultiplier { get; set; } = 0.5f;
-        public static float HardMultiplier { get; set; } = 1f;
-        public static float GoodMultiplier { get; set; } = 1.2f;
-        public static float EasyMultiplier { get; set; } = 1.5f;
+        public static float AgainMultiplier { get; set; } = Defaults.AgainMultiplier;
+        public static float HardMultiplier { get; set; } = Defaults.HardMultiplier;
+        public static float GoodMultiplier { get; set; } = Defaults.GoodMultiplier;
+        public static float EasyMultiplier { get; set; } = Defaults.EasyMultiplier;
 
-        public static float StreakWeight { get; set; } = 0.1f;
+        public static float StreakWeight { get; set; } = Defaults.StreakWeight;
 
         public static AppSettings ToAppSettings()
         {
